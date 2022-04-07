79629b2162cc3b4f5a8aac60b8041c38d0ddf0b8
Rathin Kamble <rathink4@gmail.com>
2022-04-07 20:02:59 +0400

    Added Week-03

---
+++ Week-03/isSequence.cpp
@@ -0,0 +1,30 @@
+// Problem Name: Integer to Roman
+// Difficulty: Medium
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Is Subsequence.
+// Memory Usage: 6.2 MB, less than 88.13% of C++ online submissions for Is Subsequence.
+// g++ -std=c++17 -o your-output_exe-name source_file.cpp
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isSubsequence(string s, string t) {
+    if(s.length() == 0) return true;
+    int i = 0, j=0;
+    while(i < t.length() && t.length()){
+        if(s[j] == t[i]){
+            i++;
+            j++;
+            if(j == s.length()) return true;
+            else continue;
+        }
+        i++;
+    }
+    return false;  
+}
+
+int main(){
+    string s = "";
+    string t = "ahbgdc";
+    cout << isSubsequence(s, t);
+    return 0;
+}
