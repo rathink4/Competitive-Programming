d8a91f2969bf6f4496da7ba03a4721e108eaf099
Rathin Kamble <rathink4@gmail.com>
2022-04-12 11:24:23 +0400

    Added Week-04

---
+++ Week-04/Atoi.cpp
@@ -0,0 +1,23 @@
+// Problem Name: String to Integer (atoi)
+// Difficulty: Medium
+// Runtime: 8 ms, faster than 25.81% of C++ online submissions for String to Integer (atoi).
+// Memory Usage: 7.3 MB, less than 7.60% of C++ online submissions for String to Integer (atoi).
+// g++ -std=c++17 -o your-output_exe-name source_file.cpp
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int myAtoi(string s){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    stringstream ss(s);
+    int res = 0;
+    ss >> res;
+    return res;
+}
+
+int main(){
+    string s = "    -42";
+    cout << myAtoi(s);
+    return 0;
+}
